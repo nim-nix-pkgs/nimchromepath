e00fee242e5e7aa05e4f76a9d8a86589ceb27fe3
hugosenari <hugosenari@gmail.com>
2022-03-12 04:26:03 -0300

    chore: re index nimchromepath

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Thin lib to find if chrome exists on Windows, Mac, or Linux.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimchromepath-master".url = "path:./master";
+  inputs."nimchromepath-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimchromepath-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Thin lib to find if chrome exists on Windows, Mac, or Linux.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimchromepath-master.flake = false;
+  inputs.src-nimchromepath-master.owner = "felipetesc";
+  inputs.src-nimchromepath-master.ref   = "refs/heads/master";
+  inputs.src-nimchromepath-master.repo  = "NimChromePath";
+  inputs.src-nimchromepath-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimchromepath-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimchromepath-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
